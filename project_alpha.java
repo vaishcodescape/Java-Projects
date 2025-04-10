99b30a9b75722429d10d8f8ce85d03cb210e4f4a
Aditya Vaish <adityavaish846@gmail.com>
2025-04-11 02:05:01 +0530

    changes made to repo

---
+++ project_alpha.java
@@ -0,0 +1,7 @@
+/*Alpha_Project in Java */
+public class project_alpha {
+
+    public static void main(String[] args) {
+        System.out.println("Java is the Best programming language !");
+    }
+}
